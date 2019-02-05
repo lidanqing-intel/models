e695cbe648643d1d8f65d84a8586cd6cbe5041cf
Wojciech Uss <wojciech.uss@intel.com>
2019-02-05 02:41:49 +0100

    a futile attempt to fix Transformer

--- fluid/PaddleNLP/neural_machine_translation/transformer/scripts/save_model.sh
+++ fluid/PaddleNLP/neural_machine_translation/transformer/scripts/save_model.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+DATASET_DIR=/home/wojtuss/repos/PaddlePaddle/data/Transformer/gen_data/
+MODEL_DIR=/home/wojtuss/repos/PaddlePaddle/data/Transformer/base_model/
+# MODEL_DIR=/home/wojtuss/repos/PaddlePaddle/data/Transformer/big_model/
+SAVE_MODEL_DIR=/home/wojtuss/repos/PaddlePaddle/data/Transformer/my_base_model_mem-opt_py-reader_10_INT64_3
 cd ..
 python infer.py \
-        --src_vocab_fpath gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
-        --trg_vocab_fpath gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
+        --src_vocab_fpath $DATASET_DIR/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
+        --trg_vocab_fpath $DATASET_DIR/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
 	--special_token '<s>' '<e>' '<unk>' \
-	--test_file_pattern gen_data/wmt16_ende_data_bpe/newstest2016.tok.bpe.32000.en-de \
+	--test_file_pattern $DATASET_DIR/wmt16_ende_data_bpe/newstest2016.tok.bpe.32000.en-de \
 	--token_delimiter ' ' \
         --batch_size 32 \
-	--save_model_dir /home/wojtuss/repos/PaddlePaddle/data/Transformer/my_model \
-	model_path /home/wojtuss/repos/PaddlePaddle/data/Transformer/iter_100000.infer.model \
+	--save_model_dir $SAVE_MODEL_DIR \
+	--use_mem_opt True \
+        --use_py_reader False \
+	model_path $MODEL_DIR/iter_100000.infer.model/ \
 	beam_size 4 \
 	max_out_len 255
 
